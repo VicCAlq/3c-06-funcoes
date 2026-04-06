e0d3467834674487b8a1171285d4614a933a6d7f
beatriz-carneiro <beatriz.carneiro@nave.org.br>
2026-04-06 09:10:17 -0300

    3 questões

--- ex-01-funcoes.js
+++ ex-01-funcoes.js
@@ -321,8 +321,11 @@ a string "Bom dia!". Envie na resposta a função criada, sem executá-la (basta
 enviar o nome da função sem os parênteses)
 */
 // Escreva o código da solução abaixo:
+function saudacao() {
+  return "Bom dia!"
+}
 
-export const resposta01 = false
+export const resposta01 = saudacao
 
 /* Questão 2
 Crie uma função chamada "dobro" que recebe um número como argumento e retorna 
@@ -330,8 +333,10 @@ o dobro desse número. Envie na resposta a função criada, sem executá-la (bas
 enviar o nome da função sem os parênteses)
 */
 // Escreva o código da solução abaixo:
-
-export const resposta02 = false
+function dobro(numero) {
+  return numero * 2
+}
+export const resposta02 = dobro
 
 /* Questão 3
 Crie uma função chamada "ehPositivo" que recebe um número como argumento e 
@@ -340,8 +345,12 @@ Envie na resposta a função criada, sem executá-la (basta enviar o nome
 da função sem os parênteses)
 */
 // Escreva o código da solução abaixo:
-
-export const resposta03 = false
+function ehPositivo (outronumero)
+{if(outronumero > 0)
+  {return true}
+  else{return false}
+}
+export const resposta03 = ehPositivo
 
 /* Questão 4
 Crie uma função chamada "nomeCompleto" que recebe dois argumentos: 
